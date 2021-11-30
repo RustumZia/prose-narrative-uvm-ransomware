45e19f3ad8604ee95a736a9892a7e812796a9624
RustumZia <95311445+RustumZia@users.noreply.github.com>
2021-11-30 14:20:07 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+
+
+
+const cover = document.getElementById("article-cover");
+const coverBuffer = document.getElementById("cover-buffer");
+
+window.onresize = function() {
+    // cover.clientWidth = document.body.clientWidth
+    // console.log(cover.clientWidth);
+    coverBuffer.style.height = `${cover.clientHeight}px`
+}
+window.onresize()
+
+window.onload = window.onresize
+
+const video = document.getElementById("header-video");
+window.onscroll = (e) => {
+    cover.style.opacity = Math.min(1, 1 / (window.scrollY / 400))
+    
+    if (window.scrollY > window.innerHeight / 4) {
+        video.pause()
+    } else {
+        video.play()
+    }
+}
